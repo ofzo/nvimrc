8878b09cb34ea95c84078eb0cae2124e1e415289
roadup <roadup.cc@gmail.com>
2022-07-02 15:27:42 +0800

    comb

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,33 +11,32 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
 
-	    -- Packer can manage itself
-	    use 'wbthomason/packer.nvim'
-        use 'neovim/nvim-lspconfig'
-        use 'github/copilot.vim'
-        use 'neovim/nvim-lspconfig'
+	 -- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'github/copilot.vim'
 
-	    use {
-		        'kyazdani42/nvim-tree.lua',
-    		    requires = {
-      			        'kyazdani42/nvim-web-devicons', -- optional, for file icon
-   	 	        }
-	    }
+	use {
+	    'kyazdani42/nvim-tree.lua',
+    	requires = {
+  			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+ 	    }
+    }
 
-        use {
-                'weilbith/nvim-code-action-menu',
-                after="coc.nvim",
-                requires = "xiyaowong/coc-code-action-menu.nvim",
-                config = function()
-                         require "coc-code-action-menu"
-                end,
-                -- cmd="CodeAction"
-        }
+    use {
+        'weilbith/nvim-code-action-menu',
+        after="coc.nvim",
+        requires = "xiyaowong/coc-code-action-menu.nvim",
+        config = function()
+            require "coc-code-action-menu"
+        end,
+        -- cmd="CodeAction"
+    }
 
---        use {
---                "SmiteshP/nvim-gps",
---                requires = "nvim-treesitter/nvim-treesitter"
---        }
+--  use {
+--      "SmiteshP/nvim-gps",
+--      requires = "nvim-treesitter/nvim-treesitter"
+--  }
 
 end)
 
